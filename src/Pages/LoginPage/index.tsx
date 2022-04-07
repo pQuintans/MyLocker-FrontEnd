81545f9920cd33c40c9efc409a71ea58311582e7
pQuintans <caixote.dom@gmail.com>
2022-04-07 10:42:18 -0300

    Login screen - Password section added

--- src/Pages/LoginPage/index.tsx
+++ src/Pages/LoginPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import NavBar from '../../components/NavBar'
 
@@ -7,31 +7,59 @@ import Logo from '../../assets/LogoPainted.png'
 import './styles.scss'
 
 function LoginPage() {
+  const [email, setEmail] = useState(false)
+
   return (
     <div id='login-page'>
       <NavBar smallNav={true} />
-      <div className='form-container'>
-        <form
-          onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-            e.preventDefault()
-          }}
-        >
-          <img src={Logo} alt='MyLocker' />
-          <div className='bottom-section'>
-            <div className='content'>
-              <div className='text-container'>
-                <p className='title'>Entrar</p>
-                <p className='subtitle'>Digite seu e-mail da Unicamp</p>
+      {!email ? (
+        <div className='form-container'>
+          <form
+            onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+              e.preventDefault()
+            }}
+          >
+            <img src={Logo} alt='MyLocker' />
+            <div className='bottom-section'>
+              <div className='content'>
+                <div className='text-container'>
+                  <p className='title'>Entrar</p>
+                  <p className='subtitle'>Digite seu e-mail da Unicamp</p>
+                </div>
+                <div className='input-container'>
+                  <input type='text' placeholder='E-mail Institucional' />
+                  <p>Esqueceu seu e-mail?</p>
+                </div>
               </div>
-              <div className='input-container'>
-                <input type='text' placeholder='E-mail Institucional' />
-                <p>Esqueceu seu e-mail?</p>
-              </div>
+              <button onClick={() => setEmail(true)}>Continuar</button>
             </div>
-            <button>Continuar</button>
-          </div>
-        </form>
-      </div>
+          </form>
+        </div>
+      ) : (
+        <div className='form-container'>
+          <form
+            onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+              e.preventDefault()
+            }}
+          >
+            <img src={Logo} alt='MyLocker' />
+            <div className='bottom-section'>
+              <div className='content'>
+                <div className='text-container'>
+                  <p className='title'>Entrar</p>
+                  <p className='subtitle'>Digite sua senha para fazer login</p>
+                  <p className='email'>cl200146@g.unicamp.br</p>
+                </div>
+                <div className='input-container'>
+                  <input type='text' placeholder='E-mail Institucional' />
+                  <p>Esqueceu sua senha?</p>
+                </div>
+              </div>
+              <button onClick={() => setEmail(false)}>Continuar</button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   )
 }
