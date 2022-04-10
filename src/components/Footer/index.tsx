315ff1bacb44e03e9fd95bdbf7d0a9ea4998c972
pQuintans <caixote.dom@gmail.com>
2022-04-10 18:30:49 -0300

    Footer - Add social media links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -19,18 +19,38 @@ function Footer() {
         <img src={ShortLogoWhite} alt='MyLocker' />
         <div className='right-section'>
           <div className='logo-section'>
-            <div className='logo twitter'>
-              <AiOutlineTwitter className='twitter' />
-            </div>
-            <div className='logo instagram'>
-              <AiOutlineInstagram className='twitter' />
-            </div>
-            <div className='logo youtube'>
-              <AiFillYoutube className='twitter' />
-            </div>
-            <div className='logo email'>
-              <AiOutlineMail className='twitter' />
-            </div>
+            <a
+              href='https://twitter.com/MyLockerTCC'
+              target='_blank'
+              className='logo twitter'
+              rel='noreferrer'
+            >
+              <AiOutlineTwitter />
+            </a>
+            <a
+              href='https://instagram.com/MyLocker_TCC'
+              target='_blank'
+              className='logo instagram'
+              rel='noreferrer'
+            >
+              <AiOutlineInstagram />
+            </a>
+            <a
+              href='https://www.youtube.com/channel/UC2adqJDQcWlhAO2y85D7Lkw'
+              target='_blank'
+              className='logo youtube'
+              rel='noreferrer'
+            >
+              <AiFillYoutube />
+            </a>
+            <a
+              href='mailto:mylocker.contato@gmail.com'
+              target='_blank'
+              className='logo email'
+              rel='noreferrer'
+            >
+              <AiOutlineMail />
+            </a>
           </div>
           <Link className='link' to='/sobre-nos'>
             Sobre Nós
