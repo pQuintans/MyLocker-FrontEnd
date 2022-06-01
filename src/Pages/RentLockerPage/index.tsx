5010ac4adff2c14218bf1bf9adbea44f554f12fe
pQuintans <caixote.dom@gmail.com>
2022-05-31 22:39:32 -0300

    Start development rent lockers page

--- src/Pages/RentLockerPage/index.tsx
+++ src/Pages/RentLockerPage/index.tsx
@@ -1,17 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChooseLockersSection from '../../components/RentLocker/ChooseLockersSection'
 import Footer from '../../components/Footer'
 import NavBar from '../../components/NavBar'
 
 import './styles.scss'
 import { ChooseLocker } from '../../components/RentLocker/ChooseLocker'
+import api from '../../api'
+import { AxiosResponse } from 'axios'
 
 export type SectionsTypes = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+
+type SectionType = {
+  id: number
+  color: string
+  left_room: string
+  right_room: string
+  status: number
+}
+export interface Locker {
+  number: number
+  isRented: boolean
+  rentedAt: string | null
+  FK_section_id: number
+  status: number
+  section: SectionType
+}
 
 function RentLockerPage() {
   const [sectionChoosed, setSectionChoosed] = useState<SectionsTypes | null>(
     null
   )
+  const [lockers, setLockers] = useState<Locker[]>()
+
+  function loadSections() {
+    api
+      .get('/lockers')
+      .then((response: AxiosResponse) => {
+        setLockers(response.data)
+      })
+      .catch(err => {
+        console.log(err.response.data)
+      })
+  }
+
+  useEffect(() => {
+    loadSections()
+  }, [])
 
   return (
     <div id='rent-locker-page'>
@@ -27,6 +61,7 @@ function RentLockerPage() {
           <ChooseLockersSection navigateToSection={setSectionChoosed} />
         ) : (
           <ChooseLocker
+            lockers={lockers!}
             changeSection={setSectionChoosed}
             actualSection={sectionChoosed}
           />
--- src/components/RentLocker/ChooseLocker/index.tsx
+++ src/components/RentLocker/ChooseLocker/index.tsx
@@ -1,18 +1,43 @@
-import React, { useState } from 'react'
-import { SectionsTypes } from '../../../Pages/RentLockerPage'
+import React from 'react'
+import { Locker, SectionsTypes } from '../../../Pages/RentLockerPage'
 
 import '../styles.scss'
 
 interface ChooseLocker {
   changeSection: (sectionNumber: SectionsTypes) => void
   actualSection: number
+  lockers: Locker[]
 }
 
-export function ChooseLocker({ changeSection, actualSection }: ChooseLocker) {
+export function ChooseLocker({
+  lockers,
+  changeSection,
+  actualSection,
+}: ChooseLocker) {
+  const selectedLockers = lockers.reduce(
+    (selectedLockers: Locker[], locker) => {
+      if (locker.FK_section_id == actualSection) {
+        selectedLockers.push(locker)
+      }
+      return selectedLockers
+    },
+    []
+  )
+
+  const lockersStyle = {
+    backgroundColor: selectedLockers[0].section.color,
+  }
+
   return (
     <>
       <div id='rent-lockers' className='choose-locker'>
-        {actualSection}
+        {selectedLockers.map(locker => {
+          return (
+            <div style={lockersStyle} key={locker.number}>
+              <p>{locker.number}</p>
+            </div>
+          )
+        })}
       </div>
     </>
   )
