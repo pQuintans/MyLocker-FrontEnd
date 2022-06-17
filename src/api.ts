d8709272e0f14ba9678b303bc6299089c16ad0b5
pQuintans <caixote.dom@gmail.com>
2022-06-17 16:46:28 -0300

    Change api url

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ const development = process.env.NODE_ENV !== 'production'
 const api = axios.create({
   baseURL: development
     ? 'http://localhost:3333/'
-    : 'https://mylocker-backend.herokuapp.com/',
+    : 'https://mylocker-api-production.up.railway.app/',
 })
 
 export default api
