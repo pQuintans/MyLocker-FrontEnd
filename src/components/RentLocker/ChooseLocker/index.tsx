0ec8dfcad7a20854aa69bd2a358f0e91f56fff88
pQuintans <caixote.dom@gmail.com>
2022-06-18 22:04:16 -0300

    Add loading functionality on chooseLockers page

--- src/components/RentLocker/ChooseLocker/index.tsx
+++ src/components/RentLocker/ChooseLocker/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import SectionsMap from '../LockersMap'
 
@@ -10,6 +10,7 @@ import LockerImage from '../../../assets/LockerImage.png'
 import '../styles.scss'
 import './styles.scss'
 import { useDarkTheme } from '../../../hooks/useDarkTheme'
+import { Loading } from '../../Loading/Loading'
 
 interface ChooseLocker {
   changeSection: (sectionNumber: SectionsTypes) => void
@@ -26,6 +27,12 @@ export function ChooseLocker({
 }: ChooseLocker) {
   const { darkTheme } = useDarkTheme()
   const lockersRef = useRef<HTMLDivElement>(null)
+  const [lockersStyle, setLockersStyle] = useState<{
+    backgroundColor: string
+  }>({ backgroundColor: '' })
+  const [selectedLockers, setSelectedLockers] = useState<Locker[][] | null>(
+    null
+  )
 
   function splitArrayInPieces(lockers: Locker[], tamanho: number) {
     const lockersSplited = []
@@ -37,19 +44,27 @@ export function ChooseLocker({
     return lockersSplited
   }
 
-  const selectedLockers = splitArrayInPieces(
-    lockers.reduce((selectedLockers: Locker[], locker) => {
-      if (locker.FK_section_id == actualSection) {
-        selectedLockers.push(locker)
-      }
-      return selectedLockers
-    }, []),
-    8
-  )
+  useEffect(() => {
+    if (lockers != null) {
+      setSelectedLockers(
+        splitArrayInPieces(
+          lockers.reduce((selectedLockers: Locker[], locker) => {
+            if (locker.FK_section_id == actualSection) {
+              selectedLockers.push(locker)
+            }
+            return selectedLockers
+          }, []),
+          8
+        )
+      )
+    }
+  }, [lockers, actualSection])
 
-  const lockersStyle = {
-    backgroundColor: selectedLockers[0][0].section.color,
-  }
+  useEffect(() => {
+    if (selectedLockers) {
+      setLockersStyle({ backgroundColor: selectedLockers[0][0].section.color })
+    }
+  }, [selectedLockers])
 
   return (
     <>
@@ -57,40 +72,47 @@ export function ChooseLocker({
         id='rent-lockers'
         className={(darkTheme ? 'dark' : '') + ' choose-locker '}
       >
-        <div className='top-section'>
-          <p>{selectedLockers[0][0].section.left_room}</p>
-          <div className='lockers' ref={lockersRef}>
-            {selectedLockers.map((lockersParsed, index) => {
-              return (
-                <div className='lockers-parsed' key={index}>
-                  {lockersParsed.map(locker => {
-                    return (
-                      <div
-                        className={
-                          'img-container' + (locker.isRented ? ' rented' : '')
-                        }
-                        key={locker.number}
-                        style={lockersStyle}
-                      >
-                        <img
-                          src={LockerImage}
-                          onClick={() => {
-                            selectLocker(locker)
-                          }}
-                        />
-                      </div>
-                    )
-                  })}
-                </div>
-              )
-            })}
-          </div>
-          <p>{selectedLockers[0][0].section.right_room}</p>
-        </div>
-        <SectionsMap
-          actualSection={actualSection}
-          navigateToSection={changeSection}
-        />
+        {selectedLockers ? (
+          <>
+            <div className='top-section'>
+              <p>{selectedLockers[0][0].section.left_room}</p>
+              <div className='lockers' ref={lockersRef}>
+                {selectedLockers.map((lockersParsed, index) => {
+                  return (
+                    <div className='lockers-parsed' key={index}>
+                      {lockersParsed.map(locker => {
+                        return (
+                          <div
+                            className={
+                              'img-container' +
+                              (locker.isRented ? ' rented' : '')
+                            }
+                            key={locker.number}
+                            style={lockersStyle}
+                          >
+                            <img
+                              src={LockerImage}
+                              onClick={() => {
+                                selectLocker(locker)
+                              }}
+                            />
+                          </div>
+                        )
+                      })}
+                    </div>
+                  )
+                })}
+              </div>
+              <p>{selectedLockers[0][0].section.right_room}</p>
+            </div>
+            <SectionsMap
+              actualSection={actualSection}
+              navigateToSection={changeSection}
+            />
+          </>
+        ) : (
+          <Loading />
+        )}
       </div>
     </>
   )
